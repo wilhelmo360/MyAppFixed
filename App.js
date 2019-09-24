2ea8bcbf05aa8bd26242e1c0614cdf664f96894f
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2019-09-24 14:00:45 +0300

    .

--- App.js
+++ App.js
@@ -1,58 +1,15 @@
 /* eslint-disable max-len */
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
-import List from './components/List';
+import {MediaProvider} from './contexts/MediaContext';
+import Navigator from './navigators/Navigator';
 
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/162',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/163',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <List mediaArray={mediaArray}></List>
-    </View>
+    <MediaProvider>
+      <Navigator></Navigator>
+    </MediaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    paddingHorizontal: 10,
-    paddingTop: 40,
-  },
-});
 
 export default App;
