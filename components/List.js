9b7be3d8c21396849edd596000985e52b7a64953
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2019-09-24 13:59:39 +0300

    .

--- components/List.js
+++ components/List.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {
   FlatList,
 } from 'react-native';
 import ListItem from './ListItem';
+import {MediaContext} from '../contexts/MediaContext';
+
+const useFetch = (url) => {
+  const [media, setMedia] = useContext(MediaContext);
+  const [loading, setLoading] = useState(true);
+  const fetchUrl = async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setMedia(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [media, loading];
+};
 
 const List = (props) => {
-  console.log(props);
+  const {navigation} = props;
+  const [media, loading] = useFetch('http://media.mw.metropolia.fi/wbma/media/');
+  console.log(loading);
+  console.log('media', media);
   return (
     <FlatList
-      data={props.mediaArray}
-      renderItem={({item}) => <ListItem singleMedia={item} />}
+      data={media}
+      renderItem={({item}) =>
+        <ListItem navigation={navigation} singleMedia={item} />}
+      keyExtractor={(item, index) => index.toString()}
     />
   );
 };
 
 List.propTypes = {
-  mediaArray: PropTypes.array,
+  navigation: PropTypes.object,
 };
 
 export default List;
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
+const getThumbnail = (url) => {
+  console.log('urli', url);
+  const [thumbnails, setThumbnails] = useState({});
+  async function fetchUrl() {
+    console.log('fetsurl');
+    const response = await fetch('http://media.mw.metropolia.fi/wbma/media/' + url);
+    const json = await response.json();
+    console.log('json', json);
+    setThumbnails(json.thumbnails);
+  }
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return thumbnails;
+};
+
 const ListItem = (props) => {
+  const {navigation, singleMedia} = props;
+  const tn = getThumbnail(singleMedia.file_id);
+  console.log('thumbnails', tn);
   return (
-    <TouchableOpacity style={styles.row}>
+    <TouchableOpacity
+      style={styles.row}
+      onPress={
+        () => {
+          console.log('klik');
+          navigation.push('Single', {file: singleMedia});
+        }
+      }
+    >
       <View style={styles.imagebox}>
-        <Image
+        {tn && <Image
           style={styles.image}
-          source={{uri: props.singleMedia.thumbnails.w160}}
-        />
+          source={{uri: 'http://media.mw.metropolia.fi/wbma/uploads/' + tn.w160}}
+        />}
       </View>
       <View style={styles.textbox}>
-        <Text style={styles.listTitle}> {props.singleMedia.title} </Text>
-        <Text> {props.singleMedia.description} </Text>
+        <Text style={styles.listTitle}> {singleMedia.title} </Text>
+        <Text> {singleMedia.description} </Text>
       </View>
     </TouchableOpacity>
   );
@@ -47,6 +74,7 @@ const styles = StyleSheet.create({
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
+  navigation: PropTypes.object,
 };
 
 export default ListItem;
